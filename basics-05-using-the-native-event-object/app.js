6f27328cc17949a5b2b0d67a48a397aad9bc383b
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2024-02-01 17:21:10 +0100

    Working with Watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
-            name: ''
+            name: '',
+            //fullName: '',
+            lastName: '',
         };
     },
     methods: {
@@ -22,11 +24,35 @@ const app = Vue.createApp({
     },
     computed: {
         fullName() {
-            if (this.name === '') {
+            if (this.name === '' || this.lastName === '') {
                 return '';
             }
-            return this.name + ' ' + 'Putkova';
+            return this.name + ' ' + this.lastName;
         }
+    },
+    watch: {
+        counter(value){
+            if (value > 50){
+                const that = this;
+                setTimeout(function (){
+                    that.counter = 0;
+                }, 2000);
+            }
+        }
+        // name(value) {
+        //     if (value === '') {
+        //         this.fullName = '';
+        //     } else {
+        //         this.fullName = value + ' ' + this.lastName;
+        //     }
+        // },
+        // lastName(value){
+        //     if (value === '') {
+        //         this.fullName = '';
+        //     } else {
+        //         this.fullName = this.name + ' ' + value;
+        //     }
+        // }
     }
 });
 
