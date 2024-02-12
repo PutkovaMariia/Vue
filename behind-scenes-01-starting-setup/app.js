c8655e90d066a0fd8785f9e45010ac7806a1d27e
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2024-02-12 18:52:13 +0100

    Vue Reactivity: A Deep Dive

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,46 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+////////////
+/*
+//js by default is not reactive
+let message = 'hello';
+let longMessage = message + ' world';
+console.log(longMessage);//hello world
+message = 'hello !!!!!!!';
+console.log(longMessage);//hello world
+*/
+
+const data = {
+  message: 'hello',
+  longMessage: 'hello world'
+};
+const handler = {
+  set(target, key, value){
+    if (key === 'message'){
+      target.longMessage = value + ' world';
+    }
+    target.message = value;
+  }
+};
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'hello!!!!!!';
+
+console.log(proxy.longMessage);//hello!!!!!! world
