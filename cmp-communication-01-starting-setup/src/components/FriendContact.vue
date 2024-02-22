700234ba7981df6f51f7e77797858c0b8fefc5ed
Mariia Putkova <mashaputkova@gmail.com>
2024-02-22 16:29:15 +0100

    Working with Dynamic Prop Values

--- cmp-communication-01-starting-setup/src/components/FriendContact.vue
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{friendIsFavourite === '1' ? '(Favourite)' : ''}}</h2>
+    <h2>{{ name }} {{friendIsFavourite ? '(Favourite)' : ''}}</h2>
     <button @click="toggleFavourite"> Toggle Favourite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
@@ -18,21 +18,32 @@
 
 <script>
 export default {
-  props: [
-      'name',
-      'phoneNumber',
-      'emailAddress',
-      'isFavourite'
-  ],
+  //props: ['name', 'phoneNumber', 'emailAddress', 'isFavourite'],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function (value){
+      //   return value === '1' || value === '0';
+      // }
+    },
+},
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
       friendIsFavourite: this.isFavourite,
     };
   },
@@ -41,11 +52,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavourite(){
-      if (this.friendIsFavourite === '1'){
-        this.friendIsFavourite = '0';
-      } else {
-        this.friendIsFavourite = '1';
-      }
+      this.friendIsFavourite = !this.friendIsFavourite;
     }
   }
 };
