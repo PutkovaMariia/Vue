6067e9533772a7e71b8da5d3a131d5d4f7469167
Mariia Putkova <mashaputkova@gmail.com>
2024-03-28 15:46:11 +0100

    Teleport

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,13 +1,39 @@
 <script>
-export default {
+import ErrorAlert from "@/components/ErrorAlert.vue";
 
+export default {
+  components: {ErrorAlert},
+  data() {
+    return {
+      inputValid: false
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputValid = true;
+      }
+    },
+    confirmError() {
+      this.inputValid = false;
+    }
+  }
 }
 </script>
 
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal"/>
+    <button @click="setGoal">Set goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputValid">
+        <h2>Input is invalid</h2>
+        <p>Please, enter something</p>
+        <button @click="confirmError">OK</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
