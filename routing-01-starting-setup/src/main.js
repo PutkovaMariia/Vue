8b0a232b1d4742ae55637db588330683037741ed
Mariia Putkova <mashaputkova@gmail.com>
2024-04-22 18:32:22 +0200

    Redirecting & "Catch All" Routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -9,9 +9,17 @@ import TeamMembers from "@/components/teams/TeamMembers.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {path: '/', redirect: '/teams'},
+        // or we can redirect it inside the path that we want to
+        // redirect to (with redirect the url is changed,
+        // in alias url is the same)
+        //{path: '/teams', component: TeamsList, alias: '/'}, //our-domain.com/teams => TeamsList
         {path: '/teams', component: TeamsList}, //our-domain.com/teams => TeamsList
         {path: '/users', component: UsersList}, //our-domain.com/users => UsersList
         {path: '/teams/:teamId', component: TeamMembers, props: true},
+        //if no of this routes handling users input we can
+        // set our reserved page or any another component that we want
+        {path: '/:notFound(.*)', redirect: '/teams'},
     ],
     linkActiveClass: 'active'
 });
