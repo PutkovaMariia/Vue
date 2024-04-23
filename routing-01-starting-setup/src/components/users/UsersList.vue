b3674b156df950937ae106bb1a88e7a95f7bb683
Mariia Putkova <mashaputkova@gmail.com>
2024-04-23 16:43:44 +0200

    Beyond Entering: Route Leave Guards

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,17 +14,38 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {changesSaved: false}
+  },
   methods: {
-    confirmInput(){
+    confirmInput() {
       //do something
       //in the end we want to navigate to another page
       this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
     }
   },
-  beforeRouteEnter(to, from, next){
+  beforeRouteEnter(to, from, next) {
     console.log('UsersList component cmp beforeRouteEnter');
     console.log(to, from);
     next();
+  },
+  beforeRouteLeave(to, from, next) {//the router calls it first before enter guards
+    console.log('usersList cmp beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('are you sure? you got unsaved changes');
+      next(userWantsToLeave);
+    }
+  },
+  unmounted() {
+    //if user wants to leave the page,
+    //but it gives us no way of cancelling the navigation
+    console.log('unmounted');
   }
 };
 </script>
