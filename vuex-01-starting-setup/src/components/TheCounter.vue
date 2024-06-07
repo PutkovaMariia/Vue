55a93854dbb047003ae35d9fd7f8cd2dcb315c4e
Mariia Putkova <mashaputkova@gmail.com>
2024-06-07 19:29:39 +0200

    creating store

---
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  computed:{
+    counter(){
+      return this.$store.state.counter
+    }
+  },
+}
+</script>
+
+<template>
+<h3>{{counter}}</h3>
+</template>
+
+<style scoped>
+
+</style>
