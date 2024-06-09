ff559ee93a3b34d0ce4c36cb8d9033b42bd88a38
Mariia Putkova <mashaputkova@gmail.com>
2024-06-09 16:08:44 +0200

    vuex mutations

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -3,17 +3,20 @@
     <h3>{{counter}}</h3>
     <TheCounter/>
     <button @click="addOne">Add 1</button>
+    <ChangeCounter/>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from "@/components/TheCounter.vue";
+import ChangeCounter from '@/components/ChangeCounter.vue';
 
 export default {
   components: {
     BaseContainer,
-    TheCounter
+    TheCounter,
+    ChangeCounter
   },
   computed:{
     counter(){
@@ -22,7 +25,7 @@ export default {
   },
   methods:{
     addOne(){
-      this.$store.state.counter++;
+      this.$store.commit('increment');
     }
   }
 };
