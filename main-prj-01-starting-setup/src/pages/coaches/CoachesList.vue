1200da033d0fc88c8efb6aff1cbb70b82087c607
Mariia Putkova <mashaputkova@gmail.com>
2024-06-25 18:11:37 +0200

    Working on the Main Layout & Styling

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,12 @@ export default {
 </script>
 
 <template>
-
+<section>
+  FILTER
+</section>
+  <section>
+    LIST OF COACHES
+  </section>
 </template>
 
 <style scoped>
--- main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
+++ main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
@@ -1,7 +1,5 @@
 <script>
-export default {
-  name: 'RequestsReceived'
-};
+
 </script>
 
 <template>
